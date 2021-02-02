7c49910eed1fb8c9e5b183d07388d2e3a3469da5
Polygon228 <sholohroman155@gmail.com>
2021-02-02 15:13:11 +0200

    C:/Program Files/Git/

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,43 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Клас що являє собою завдання
+ * @author Roman
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+    
+    /**
+     * Знаходить в масиві число, яке повторюється найбільшу кількість разів
+     * @param arr вхідний масив
+     * @return число, яке повторюється найбільшу кількість разів
+     */
+    public static int Calculate(int[] arr) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i:arr){
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            }
+            else {
+             map.put(i, 1);
+            }
+        }
+        
+        int max = 1;
+        int res = 0;
+        
+        for (Map.Entry<Integer, Integer> currentEntry : map.entrySet()) {
+            if (currentEntry.getValue() > max) {
+                max = currentEntry.getValue();
+                res = currentEntry.getKey();
+            }
+        }
+        return res;
 }
+    
+}
+
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,8 +5,8 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
+	int[] arr = {10,6,4,3,2,3,0};
+        System.out.printf("the number that repeats the largest number of times: %d",Exercise.Calculate(arr));      
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
     }
 }
